ac55935f49844f836662648e1fc5f98e0bb2a34c
Rec <recrumptor@gmail.com>
2025-07-15 23:40:34 +0500

    Update feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -7,10 +7,10 @@ cat >> ./feeds.conf.default <<EOF
 # src-git small https://github.com/kenzok8/small
 
 # Extra Packages
-src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
+# src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
 # src-git lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
-src-git modemband https://github.com/4IceG/luci-app-modemband
-src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
+# src-git modemband https://github.com/4IceG/luci-app-modemband
+# src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
 # src-git sms_tool https://github.com/4IceG/luci-app-sms-tool
 # src-git xray https://github.com/yichya/luci-app-xray
 # src-git v2raya https://github.com/v2rayA/v2raya-openwrt
