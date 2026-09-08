59d2133a05666b7fe4987876afa62facb11dfc78
Rec <197471105+recrumptor@users.noreply.github.com>
2026-09-08 22:05:57 +0500

    Update notion_wireless.sh

--- custom/files/etc/uci-defaults/notion_wireless.sh
+++ custom/files/etc/uci-defaults/notion_wireless.sh
@@ -3,32 +3,42 @@
 # Copyright (c) 2023 LENAR
 #
 
-MD5=$(head /dev/mtd2 | md5sum | cut -c-6)
+C2G="$(( 1 + (RANDOM % 3) * 5 ))"
+C5G="$(( 36 + (RANDOM % 4) * 4 ))"
+MD5="$(md5sum /dev/mtd2 | head -c 6)"
+ODR="$(sed -n "/DISTRIB_RELEASE/s/.*='\([0-9]*\).*/\1/p" /etc/openwrt_release)"
 
-for RADIO in "radio0" "radio1"
-do
+for RADIO in "radio0" "radio1"; do
   uci -q get wireless."$RADIO" || continue
-  # SSID=$(uci -q get wireless.default_$RADIO.ssid)
+
+  SSID=$(uci -q get wireless.default_$RADIO.ssid)
   BAND=$(uci -q get wireless.$RADIO.band)
-  # [ "$SSID" != "OpenWrt" ] && continue
-  uci set wireless."$RADIO".disabled="0"
+
+  [ "$SSID" != "OpenWrt" ] && continue
+
+  if [ "$ODR" -lt 25 ]; then
+    uci set wireless."$RADIO".disabled="0"
+  else
+    uci set wireless.default_"$RADIO".disabled="0"
+  fi
+
   uci set wireless."$RADIO".cell_density="0"
-  uci set wireless.default_"$RADIO".disabled="0"
-  if [ "$BAND" = "2g" ];then
+
+  if [ "$BAND" = "2g" ]; then
     uci set wireless.default_"$RADIO".ssid="Notion-${MD5}"
     uci set wireless.default_"$RADIO".ifname="wifi-2g"
-    # uci set wireless."$RADIO".channel="1"
+    uci set wireless."$RADIO".channel="${C2G}"
   else
     uci set wireless.default_"$RADIO".ssid="Notion-${MD5}-5G"
     uci set wireless.default_"$RADIO".ifname="wifi-5g"
-    # uci set wireless."$RADIO".channel="36"
+    uci set wireless."$RADIO".channel="${C5G}"
   fi
+
   uci set wireless.default_"$RADIO".encryption="psk2"
   uci set wireless.default_"$RADIO".key="OP${MD5}"
-  # uci set wireless.default_"$RADIO".macaddr="random"
   uci set wireless.default_"$RADIO".skip_inactivity_poll="1"
   uci set wireless.default_"$RADIO".disassoc_low_ack="0"
-  # uci set wireless.default_"$RADIO".hidden="1"
+
   uci -q commit wireless
 done
 
