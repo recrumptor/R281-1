af8349d94962c61dbe5d46fec2b27ae2270ebc8a
Rec <197471105+recrumptor@users.noreply.github.com>
2026-09-14 20:34:48 +0500

    Update diy_v2.sh

--- scripts/diy_v2.sh
+++ scripts/diy_v2.sh
@@ -3,11 +3,6 @@
 echo "Move Custom Files"
 mv -f ./custom/files ./
 
-echo "Fix Executable Permissions"
-chmod +x ./files/usr/bin/antenna-switch
-chmod +x ./files/etc/init.d/antenna
-chmod +x ./files/etc/uci-defaults/99-antenna-enable
-
 echo "Patch For NCM"
 mv -f ./custom/patch/ncm.* ./package/network/utils/comgt/files/
 
